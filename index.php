eeffa9844d0ab6acc17acabbe1f2ccdaa6d03fe8
dlord <dlord@outlook.com>
2015-07-31 09:31:23 -0400

    Structured HTML file

--- index.php
+++ index.php
@@ -3,7 +3,23 @@
 require_once 'ColorPalette.php';
 
 ?>
-<ul style="margin:0; padding:0;">
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>ColorPalette example</title>
+        <style>
+            .color-list {
+                margin:0;
+                padding:0;
+            }
+            .color-block {
+                padding: 20px;
+            }
+        </style>
+    </head>
+    <body>
+        <ul class="color-list">
 <?php
 
 $color_generator = new ColorPalette('#cc44ff');
@@ -11,11 +27,14 @@ $color_generator = new ColorPalette('#cc44ff');
 for ( $i = 0; $i < 10; $i++) {
 
     $color = $color_generator->render();
+
 ?>
-    <li style="padding: 20px; background-color: <?php echo $color ?>"></li>
+            <li class="color-block" style="background-color: <?php echo $color ?>"></li>
 <?php
 
 }
 
 ?>
-</ul>
+        </ul>
+    </body>
+</html>
